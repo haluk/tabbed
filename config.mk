e97a47b5c58219920fd21fd392c8eb84bd74715a
Haluk Dogan <haluk.dogan@huskers.unl.edu>
2020-08-15 23:31:51 -0500

    config

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 0.6
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = $(HOME)/.build/tabbed
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
